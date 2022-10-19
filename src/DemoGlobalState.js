15f8a2613da0f5fb491d1876c4751af094d68fc3
tribt <bienthanhtri@gmail.com>
2022-10-19 16:49:16 +0700

    fix issue html tag

--- src/DemoGlobalState.js
+++ src/DemoGlobalState.js
@@ -7,23 +7,22 @@ function DemoGlobalState() {
   return (
     <div>
       <h1>Demo Global State = useReducer() + context/useContext()</h1>
-      <p>
-        Redux is the best choice for global state
-        <ul>
-          <li>
-            performance, context & useReducer will render all children when
-            state changes
-          </li>
-          <li>
-            redux can be used in Javascript project, react-context is only used
-            for react project
-          </li>
-          <li>
-            Redux can add many middleware such as logger, with context we have
-            to manually implement middleware
-          </li>
-        </ul>
-      </p>
+      <p>Redux is the best choice for global state </p>
+      <ul>
+        <li>
+          performance, context & useReducer will render all children when state
+          changes
+        </li>
+        <li>
+          redux can be used in Javascript project, react-context is only used
+          for react project
+        </li>
+        <li>
+          Redux can add many middleware such as logger, with context we have to
+          manually implement middleware
+        </li>
+      </ul>
+
       <input
         type="text"
         name="todo"
